53fd80a7022156baf4cf03ddf69405b902a772e7
Kurt Riecken <kurt.riecken@gmail.com>
2023-11-04 07:57:32 -0400

    added blogpost update route; functioning

--- controllers/api/blog-post-routes.js
+++ controllers/api/blog-post-routes.js
@@ -41,6 +41,29 @@ router.post('/', async (req, res) => {
     }
 });
 
+// UPDATE BlogPost by ID
+router.put('/:id', async (req, res) => {
+    try {
+        const blogPostData = await BlogPost.update(
+            {
+                title: req.body.title,
+                content: req.body.content,
+                date_created: req.body.date_created,
+                user_id: req.body.user_id
+            },
+            {
+                where: {
+                    id: req.params.id,
+                }
+            }
+        );
+
+        res.status(200).json(blogPostData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 // DELETE BlogPost by id
 router.delete('/:id', async (req, res) => {
     try {
@@ -50,7 +73,7 @@ router.delete('/:id', async (req, res) => {
             },
         });
 
-        if(!blogPostData) {
+        if (!blogPostData) {
             res.status(404).json({ message: "No blog post found with that ID!" });
             return;
         };
