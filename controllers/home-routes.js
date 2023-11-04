61a8c5cdb49fabcad3cd4b065420e31494163981
Kurt Riecken <kurt.riecken@gmail.com>
2023-11-04 16:23:00 -0400

    added login/signup pages with some links

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -8,7 +8,27 @@ router.get('/', async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
+router.get('/login', async (req, res) => {
+    // If user is logged in, redirect to their profile
+    // BUILD THIS OUT
+    if (req.session.logged_in) {
+        // BUILD THIS
+        return;
+    }
+    else {
+        // Send them to the login page
+        res.render('login');
+    }
+});
+
+router.get('/signup', async (req, res) => {
+        // Send them to the signup page
+        // You shouldn't be able to get here except through clicking the 
+        // "signup" button on the homepage or login screen
+        res.render('signup');
+});
 
 router.get('*', async (req, res) => {
     try {
